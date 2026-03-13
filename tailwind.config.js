b5626fa0150d7e8fea9ded6276ca971cae3fae74
Sabrina Blais <sabrina.blaistif@gmail.com>
2026-03-13 19:22:02 -0400

    colours for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      colors: {
+        primary: { 50: '#f0f2f1', 500: '#B8C3AE', 900: '#8A9683' },
+        secondary: '#EDE4E0',  // Beige accent
+        navy: '#4B6F88'  // Text/buttons
+      }
+    }
+  }
 }
-
